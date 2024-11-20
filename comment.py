490b207dbe698240b0c6a50c7bc58fc4c52f8974
Aris Rusliana <aris.rusliana@gmail.com>
2024-11-20 16:49:42 +0700

    python course

---
+++ comment.py
@@ -0,0 +1,9 @@
+print("pada python, comment bisa menggunakan # atau '''blablabla'''")
+
+#print("Ini adalah comment single line")
+
+'''
+    comment multiline bisa menggunakan
+    single quote atau
+    double quote
+'''
